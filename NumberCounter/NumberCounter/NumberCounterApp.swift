144fa5067e928ff237d2f05b03593156759ea5a7
Devki <hanmix2003@naver.com>
2023-11-09 07:16:16 +0900

    Initial Commit
    프로젝트 초기 설정 관련 Commit

---
+++ NumberCounter/NumberCounter/NumberCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCounterApp.swift
+//  NumberCounter
+//
+//  Created by mingmac on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct NumberCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
